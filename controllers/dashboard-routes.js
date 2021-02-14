1e177ac6eb059ed928737ea140a4234ddd183c34
Nick May <nickmay9@outlook.com>
2021-02-14 14:35:51 -0500

    go to single post. adding edit features to post

--- controllers/dashboard-routes.js
+++ controllers/dashboard-routes.js
@@ -43,6 +43,50 @@ router.get('/', (req, res) => {
     });
 });
 
-router.get('/edit/:id', (req, res) => {});
+router.get('/post/:id', (req, res) => {
+    Post.findOne({
+        where: {
+            id: req.params.id
+        },
+        attributes: [
+            'id',
+            'title',
+            'post_text',
+            'city',
+            'country',
+            'created_at'
+        ],
+        include: [
+            {
+                model: Comment,
+                attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+                include: {
+                    model: User,
+                    attributes: ['username']
+                }
+            },
+            {
+                model: User,
+                attributes: ['id', 'username']
+            }
+        ]
+    }).then(dbPostData => {
+        if(!dbPostData){
+            res.status(404).json({ message: 'No post found with this id.' });
+            return;
+        }
+
+        const post = dbPostData.get({ plain: true });
+
+        res.render('single-post', { 
+            post,
+            loggedIn: req.session.loggedIn,
+            user_id: req.session.user_id
+        });
+    }).catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
 
 module.exports = router;
