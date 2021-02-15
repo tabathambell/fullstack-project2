5e8bf4c6c85011419aa15a5ae62c93f259ceab2e
Racer <reaganjoseph26@yahoo.com>
2021-02-14 19:22:01 -0500

    edit post link error

--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -91,27 +91,27 @@ router.post('/', (req, res) => {
       });
 });
 
-router.put('/:id', (req, res) => {
-    Post.update(
-        {
-            title: req.body.title
-        },
-        {
-            where: {
-                id: req.params.id
-            }
-        }
-    ).then(dbPostData => {
-        if(!dbPostData){
-            res.status(404).json({ message: 'No post found with this id.' });
-            return;
-        }
-        res.json(dbPostData);
-    }).catch(err => {
-        console.log(err);
-        res.status(500).json(err);
-    });
-});
+// router.put('/:id', (req, res) => {
+//     Post.update(
+//         {
+//             title: req.body.title
+//         },
+//         {
+//             where: {
+//                 id: req.params.id
+//             }
+//         }
+//     ).then(dbPostData => {
+//         if(!dbPostData){
+//             res.status(404).json({ message: 'No post found with this id.' });
+//             return;
+//         }
+//         res.json(dbPostData);
+//     }).catch(err => {
+//         console.log(err);
+//         res.status(500).json(err);
+//     });
+// });
 
 router.delete('/:id', (req, res) => {
     Post.destroy({
--- public/javascript/edit-post.js
+++ public/javascript/edit-post.js
@@ -1,14 +1,20 @@
 async function editFormHandler(event) {
     event.preventDefault();
   
-    const title = document.querySelector('input[name="post-title"]').value.trim();
+    const title = document.querySelector('input[name="postTitle"]').value.trim();
+    const post_text = document.querySelector('input[name="postBody"]').value.trim();
+    const city = document.querySelector('input[name="postCity"]').value.trim();
+    const country = document.querySelector('input[name="postCountry"]').value.trim();
     const id = window.location.toString().split('/')[
       window.location.toString().split('/').length - 1
     ];
-    const response = await fetch(`/api/posts/${id}`, {
+    const response = await fetch(`/dashboard/edit/${id}`, {
       method: 'PUT',
       body: JSON.stringify({
-        title
+        title,
+        post_text,
+        city,
+        country
       }),
       headers: {
         'Content-Type': 'application/json'
