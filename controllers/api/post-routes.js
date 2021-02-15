0157b9b7362940f1a8bfe3fc5e9f70e3c2e04d05
Racer <reaganjoseph26@yahoo.com>
2021-02-14 20:28:58 -0500

    edit post link fixed

--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -91,27 +91,32 @@ router.post('/', (req, res) => {
       });
 });
 
-// router.put('/:id', (req, res) => {
-//     Post.update(
-//         {
-//             title: req.body.title
-//         },
-//         {
-//             where: {
-//                 id: req.params.id
-//             }
-//         }
-//     ).then(dbPostData => {
-//         if(!dbPostData){
-//             res.status(404).json({ message: 'No post found with this id.' });
-//             return;
-//         }
-//         res.json(dbPostData);
-//     }).catch(err => {
-//         console.log(err);
-//         res.status(500).json(err);
-//     });
-// });
+router.put('/:id', (req, res) => {
+    Post.update(
+        {
+            title: req.body.title,
+            post_text: req.body.post_text,
+            city: req.body.city,
+            country: req.body.country,
+            user_id: req.session.user_id
+
+        },
+        {
+            where: {
+                id: req.params.id
+            }
+        }
+    ).then(dbPostData => {
+        if(!dbPostData){
+            res.status(404).json({ message: 'No post found with this id.' });
+            return;
+        }
+        res.json(dbPostData);
+    }).catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
 
 router.delete('/:id', (req, res) => {
     Post.destroy({
