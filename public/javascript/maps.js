0ae9cfcbcffc2e26f82ad2d1be6c536f6ae7d584
Racer <reaganjoseph26@yahoo.com>
2021-02-15 19:23:40 -0500

    code cleaned up a bit

--- public/javascript/maps.js
+++ public/javascript/maps.js
@@ -44,13 +44,3 @@ function initMap() {
 }
 
 mapHandler();
-
-  // window.onload = function() {
-  //     var startPos;
-  //     var geoSuccess = function(position) {
-  //       startPos = position;
-  //       document.getElementById('startLat').innerHTML = startPos.coords.latitude;
-  //       document.getElementById('startLon').innerHTML = startPos.coords.longitude;
-  //     };
-  //     navigator.geolocation.getCurrentPosition(geoSuccess);
-  // };
