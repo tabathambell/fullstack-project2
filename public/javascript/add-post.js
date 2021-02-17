7e8c647313d072020b2e03a9d578f408ad5aafa3
Tabatha Bell <tabathambell@live.com>
2021-02-17 18:10:49 -0500

    fixed width issue on homepage handlebars

--- public/javascript/add-post.js
+++ public/javascript/add-post.js
@@ -7,14 +7,8 @@ async function newPostHandler(event){
     const post_text = document.querySelector('#postBody').value.trim();
     const city = document.querySelector('#postCity').value.trim();
     const country = document.querySelector('#postCountry').value.trim();
-<<<<<<< HEAD
 
     if(title && post_text && city && country) {
-=======
-    // const image = document.querySelector('#postImage').files;
-
-    if(title && post_text && city && country ) {
->>>>>>> 508cc8e54f52851f07afbc65d9f574d7a8549db3
 
         let lat, long;
         const cityData = await fetch(`http://www.mapquestapi.com/geocoding/v1/address?key=pjtMs45lJi90EGCVfaBEDChiCmQFtGmI&location=${city}+${country}`).then(data => {
